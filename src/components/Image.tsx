b83b6a3fe5d5043dc66894e34adf65cf6c3a76ea
Maria Eduarda Xavier Vidigal <pro.maduxavier@gmail.com>
2025-07-22 19:05:08 -0300

    adiciona brownie blondie

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,16 +1,12 @@
-import { useState } from 'react';
-import fallbackImg from '/assets/fallback-img.png';
-
 type ImageProps = {
   size: string;
   src: string;
 } & React.ImgHTMLAttributes<HTMLImageElement>;
 
 export default function Image({ size, src, ...props }: ImageProps) {
-  const [source, setSource] = useState(src || fallbackImg);
   return (
     <div style={{width: size, height: size}}>
-      <img src={source} onError={() => setSource(fallbackImg)} {...props} />
+      <img src={src} {...props} />
     </div>
   );
 }
