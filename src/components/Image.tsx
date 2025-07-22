f54251a1993e6daa98fb064f88912dbddbfb7956
Maria Eduarda Xavier Vidigal <pro.maduxavier@gmail.com>
2025-07-21 22:17:52 -0300

    adiciona tratativa pra fallback img

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
+import fallbackImg from '/assets/fallback-img.png';
+
 type ImageProps = {
   size: string;
   src: string;
 } & React.ImgHTMLAttributes<HTMLImageElement>;
 
 export default function Image({ size, src, ...props }: ImageProps) {
+  const [source, setSource] = useState(src || fallbackImg);
   return (
     <div style={{width: size, height: size}}>
-      <img src={src} {...props} />
+      <img src={source} onError={() => setSource(fallbackImg)} {...props} />
     </div>
   );
 }
